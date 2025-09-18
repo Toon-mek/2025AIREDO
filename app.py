cf4e1a572834a030314b37a48347f06f7e8f45be
Toon-mek <jinghaw0912@gmail.com>
2025-09-19 06:21:38 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ from sklearn.pipeline import Pipeline
 from sklearn.linear_model import LogisticRegression
 
 st.set_page_config(page_title="Laptop Recommender", layout="wide")
-st.title("Laptop Recommender (Logistic)")
+st.title("Laptop Recommender")
 
 @st.cache_data
 def load_data():
